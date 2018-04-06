fb04b94ca3b2d8fa4466ea754009c4d3587bdb0d
Alexander Alvarez <alexander.gabriel.alvarez@Gmail.com>
2018-04-06 00:08:36 -0400

    Add files via upload

--- inputdevice.c
+++ inputdevice.c
@@ -12,12 +12,12 @@
 #define BUFF_LEN 1024			//max length of message
 
 MODULE_LICENSE("GPL");
-MODULE_AUTHOR("Erick Evangeliste, Eric Watson,Alexander Alvarez");
+MODULE_AUTHOR("Erick Evangeliste, Eric Watson, Alexander Alvarez");
 MODULE_DESCRIPTION("Assignment 3 COP4600");
 MODULE_VERSION("1.0");
 
 static int 		Major;					//major number assigned to our device driver
-char 	msg[BUFF_LEN];	//the msg the device will give when as
+char 	*msg;	//the msg the device will give when as
 static int 		counter = 0;
 int	size_of_message = 0;
 static struct 	class* charClass = NULL;
@@ -26,14 +26,14 @@ static DEFINE_MUTEX(charMutex);
 
 static int 		dev_open(struct inode *, struct file *);
 static int 		dev_release(struct inode *, struct file *);
-static ssize_t 	dev_read(struct file *, char *, size_t, loff_t *);
+
 static ssize_t 	dev_write(struct file *, const char *, size_t, loff_t *);
 
 //Operations struct
 static struct file_operations fops =
 {
 	.open = dev_open,
-	.read = dev_read,
+	//.read = dev_read,
 	.write = dev_write,
 	.release = dev_release,
 };
@@ -41,15 +41,16 @@ static struct file_operations fops =
 //this function is called when the module is loaded
 static int __init charDevice_init(void)
 {
+	mutex_init(&charMutex);
 	Major = register_chrdev(0, DEVICE_NAME, &fops);
 
 	if(Major < 0)
 	{
-		printk(KERN_ALERT "CharWrite: Registering char device failed with %d\n", Major);
+		printk(KERN_ALERT "Input: Registering char device failed with %d\n", Major);
 		return Major;	
 	}
 
-	printk(KERN_INFO "CharWrite: New Session Created: #%d\n", Major);
+	printk(KERN_INFO "Input: New Session Created: #%d\n", Major);
 
 	//register the device class
 	charClass = class_create(THIS_MODULE, CLASS_NAME);
@@ -57,11 +58,11 @@ static int __init charDevice_init(void)
 	if(IS_ERR(charClass))
 	{
 		unregister_chrdev(Major, DEVICE_NAME);
-		printk(KERN_ALERT "CharWrite: Failed to register device class\n");
+		printk(KERN_ALERT "Input: Failed to register device class\n");
 		return PTR_ERR(charClass);
 	}
 
-	printk(KERN_INFO "CharWrite: Device class created correctly\n");
+	printk(KERN_INFO "Input: Device class created correctly\n");
 
 	charDevice = device_create(charClass, NULL, MKDEV(Major, 0), NULL, DEVICE_NAME);
 
@@ -73,8 +74,8 @@ static int __init charDevice_init(void)
 		return PTR_ERR(charDevice);
 	}
 
-	printk(KERN_INFO "CharWrite: Device class created correctly");
-    mutex_init(&charMutex);
+	printk(KERN_INFO "Input: Device class created correctly");
+        
 	return SUCCESS;
 }
 
@@ -85,9 +86,9 @@ static void __exit charDevice_exit(void)
 	class_unregister(charClass);
 	class_destroy(charClass);
 	unregister_chrdev(Major, DEVICE_NAME);
-    mutex_destroy(&charMutex);
+        mutex_destroy(&charMutex);
 
-	printk(KERN_INFO "CharWrite: Goodbye from the LKM!\n");
+	printk(KERN_INFO "Input: Kernel module succesfully removed!\n");
 }
 
 //called when a process tries to open the device file, like "cat/dev/mycharfile"
@@ -95,25 +96,23 @@ static int dev_open(struct inode *inode, struct file *file)
 {
       if(!mutex_trylock(&charMutex))
     {
-        printk(KERN_ALERT "CharWrite: Device is being used by another process");
+        printk(KERN_ALERT "Input: Device is being used by another process");
         return -EBUSY;
     }
 	counter++;
-	printk(KERN_INFO "CharWrite: Device opened! Access counter: %d\n", counter);
+	printk(KERN_INFO "Input: Device opened! Access counter: %d\n", counter);
 	return 0;
 }
 
-static ssize_t dev_read(struct file * filp, char *buffer, size_t length, loff_t * offset)
-{
-	printk(KERN_ALERT "Operation not supported\n.");
-	return 0;
-}
+
 
 
 //called when a process writes to dev file: echo "hi"
 static ssize_t dev_write(struct file *filp, const char *buff, size_t length, loff_t * off)
 {
 	int i;
+
+
 
 	if((size_of_message + length) > BUFF_LEN)
 	{
@@ -123,13 +122,12 @@ static ssize_t dev_write(struct file *filp, const char *buff, size_t length, lof
 		size_of_message = BUFF_LEN;
 		if(i > 1)
 		{
-			printk(KERN_INFO "CharWrite: System has obtained %d characters from user, 0 bytes are available\n", i);
+			printk(KERN_INFO "Input: System has obtained %d characters from user, 0 bytes are available\n", i);
 			mutex_unlock(&charMutex);
 			return i;
 		}
 		else
 		{
-			mutex_unlock(&charMutex);
 			return -EFAULT;
 		}
 	}
@@ -141,7 +139,7 @@ static ssize_t dev_write(struct file *filp, const char *buff, size_t length, lof
 			sprintf(msg, "%s%s", msg, buff);
 
 		size_of_message += length;
-		printk(KERN_INFO "CharWrite: System has obtained %d characters from user, %d bytes are available\n", length, BUFF_LEN - size_of_message);
+		printk(KERN_INFO "Input: System has obtained %d characters from user, %d bytes are available\n", length, BUFF_LEN - size_of_message);
 		mutex_unlock(&charMutex);
 		return length;
 	}
@@ -151,7 +149,7 @@ static ssize_t dev_write(struct file *filp, const char *buff, size_t length, lof
 static int dev_release(struct inode *inode, struct file *file)
 {
 	mutex_unlock(&charMutex);
-	printk(KERN_INFO "CharWrite: Device successfully closed\n");
+	printk(KERN_INFO "Input: Device successfully closed\n");
 	return 0;
 }
 EXPORT_SYMBOL(size_of_message);
@@ -159,3 +157,4 @@ EXPORT_SYMBOL(msg);
 EXPORT_SYMBOL(charMutex);
 module_init(charDevice_init);
 module_exit(charDevice_exit);
+
